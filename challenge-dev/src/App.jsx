d79bc098c6cfa69b5cecc1884b4be856f339ed39
EDMR <emermontes15@gmail.com>
2024-03-31 21:17:42 -0500

    [DELETE]Lineas comentadas

--- challenge-dev/src/App.jsx
+++ challenge-dev/src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css'
 import Characters from './card/characters'
-import Nav from './components/nav'
 
 function App() {
   return (
     <div>
-    <Nav/>
     <Characters/>
     </div>
   )
